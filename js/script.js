8e60fab31a54a3efda8ffcbc3756989f43757b9a
chandra prakash <chandra385123@gmail.com>
2024-01-24 22:23:32 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/*================typing animation =========*/
+
+var typed=new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+/*================Aside =========*/
+
+const nav=document.querySelector(".nav")
